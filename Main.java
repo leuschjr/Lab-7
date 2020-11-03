08ea747913f52977f3c8b21ff34a5d3882e6b545
Jacob Leusch <leuschjr@mail.uc.edu>
2020-11-03 20:33:26 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+class Main{
+  public static void main(String[] args){
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run(){
+        new Counter();
+      }
+    });
+  }
+}
